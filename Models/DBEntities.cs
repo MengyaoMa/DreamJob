5c3d4366ae4d301a4af52f582a792a3bcf01dadb
licence <licence>
2016-04-09 16:51:08 +0200

    First commit

---
+++ Models/DBEntities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DreamJob.Models
+{
+    // DBcontext class : handles all interaction with the MySQL DB
+    public class DBEntities : DbContext
+    {
+        public DBEntities() : base(nameOrConnectionString: "LocalDBconnection") { }
+        public DbSet<Article> Article { get; set; }
+        public DbSet<Utilisateur> Utilisateurs { get; set; }
+      
+    }
+}
